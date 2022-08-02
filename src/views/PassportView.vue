fcf24ea43d7765af2385e228b4801691287980cb
Steinke, Felix <felix.steinke@cgi.com>
2022-08-02 14:18:19 +0200

    extract service urls into service.const

--- src/views/PassportView.vue
+++ src/views/PassportView.vue
@@ -53,11 +53,11 @@ import StateOfHealth from "@/components/StateOfHealth.vue";
 import ParametersOfTheBattery from "@/components/ParametersOfTheBattery.vue";
 import DismantlingProcedures from "@/components/DismantlingProcedures.vue";
 import InformationResponsibleSourcing from "@/components/InformationResponsibleSourcing.vue";
-
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 import axios from "axios";
 import {reactive} from "vue";
+import {AAS_PROXY_URL} from "@/services/service.const";
 
 export default {
   name: "PassportView",
@@ -80,23 +80,14 @@ export default {
     return {
       data: null,
       loading: true,
-      errors: [],
-      twinRegistryUrl: "http://localhost:4243",
-      aasProxyUrl: "http://localhost:4245",
+      errors: []
     };
   },
   methods: {
-    async fetchData() {
-      const res = await fetch("http://localhost:3000/334593247");
-      const data = await res.json();
-      console.log("PassportView (Data):", data);
-      return data;
-    },
     getDigitalTwinId: function (assetIds) {
       return new Promise((resolve) => {
         let encodedAssetIds = encodeURIComponent(assetIds);
-        axios.get(this.aasProxyUrl + '/lookup/shells?assetIds=' + encodedAssetIds
-        ).then((response) => {
+        axios.get(`${AAS_PROXY_URL}/lookup/shells?assetIds=${encodedAssetIds}`).then((response) => {
           console.log("PassportView (Digital Twin):", response.data);
           resolve(response.data);
         }).catch((e) => {
@@ -108,8 +99,7 @@ export default {
     getDigitalTwinObjectById: function (digitalTwinId) {
       //const res =  axios.get("http://localhost:4243/registry/shell-descriptors/urn:uuid:365e6fbe-bb34-11ec-8422-0242ac120001"); // Without AAS Proxy
       return new Promise((resolve) => {
-        axios.get(this.aasProxyUrl + '/registry/shell-descriptors/' + digitalTwinId //Calling with AAS Proxy
-        ).then((response) => {
+        axios.get(`${AAS_PROXY_URL}/registry/shell-descriptors/${digitalTwinId}`).then((response) => {
           console.log("PassportView (Digital Twin Object):", response.data);
           resolve(response.data);
         }).catch((e) => {
@@ -122,7 +112,7 @@ export default {
       //const res =  axios.get("http://localhost:8193/api/service/urn:uuid:365e6fbe-bb34-11ec-8422-0242ac120001-urn:uuid:61125dc3-5e6f-4f4b-838d-447432b97919/submodel?provider-connector-url=http://provider-control-plane:8282"); // Without AAS Proxy
       //Calling with AAS Proxy 
       return new Promise((resolve) => {
-        axios.get(this.aasProxyUrl + '/shells/' + digitalTwin.identification + '/aas/' + digitalTwin.submodelDescriptors[0].identification + '/submodel?content=value&extent=withBlobValue', {
+        axios.get(`${AAS_PROXY_URL}/shells/${digitalTwin.identification}/aas/${digitalTwin.submodelDescriptors[0].identification}/submodel?content=value&extent=withBlobValue`, {
           auth: {
             username: 'someuser',
             password: 'somepassword'
@@ -143,8 +133,6 @@ export default {
     },
   },
   async created() {
-    //this.data = await this.fetchData();
-    //console.log(data);
     let assetIds = this.$route.params.assetIds;
     this.data = await this.getPassport(assetIds);
     this.loading = false;

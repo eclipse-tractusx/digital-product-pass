a66bafec8275883ae46171129a3e85b3c18f5b13
Muhammad Saud Khan <muhammadsaud.khan@cgi.com>
2022-08-11 16:33:44 +0200

    modify keycloak local to prod settings

--- docker/consumer-ui/src/services/service.const.js
+++ docker/consumer-ui/src/services/service.const.js
@@ -5,13 +5,13 @@ const GOOGLE_CHART_API_URL = "https://chart.googleapis.com";
 const DUMMY_SERVICE = "http://localhost:3000";
 
 const INIT_OPTIONS = {
-    url: 'http://localhost:8088/auth',
+    url: 'https://centralidp.demo.catena-x.net/auth/',
     clientId: 'Cl13-CX-Battery',
     realm: 'CX-Central',
     onLoad: 'login-required'
 };
 
-const REDIRECT_URI = "http://localhost:8080/";
+const REDIRECT_URI = "https://materialpass.int.demo.catena-x.net/";
 
 
 export {TWIN_REGISTRY_URL, AAS_PROXY_URL, MOCK_AUTH_URL, GOOGLE_CHART_API_URL, DUMMY_SERVICE, INIT_OPTIONS, REDIRECT_URI};

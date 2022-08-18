61bf5e5e183e39fc8f81a7389f92bf4a1b05ee4a
Muhammad Saud Khan <muhammadsaud.khan@cgi.com>
2022-08-18 11:33:15 +0200

    format code

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@
 </template>
 
 <script>
-// import ProviderDropdown from './components/ProviderDropdown.vue'
-// import Login from './components/Login.vue'
 
 export default {
   name: "App",
@@ -17,6 +15,7 @@ export default {
   margin: 0;
   padding: 0;
 }
+
 #app {
   font-family: "Libre Franklin", sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,18 +26,21 @@ export default {
   flex-direction: column;
   min-height: 100vh;
 }
-.table-borderless > :not(caption) > * > * {
+
+.table-borderless> :not(caption)>*>* {
   border-bottom-width: 0;
   padding-left: 40px !important;
 }
+
 .table-striped tbody tr:nth-of-type(odd) {
   background-color: rgba(179, 203, 44, 0.15);
 }
+
 .table-striped tbody tr:nth-of-type(even) {
   background-color: rgba(179, 203, 44, 0.05);
 }
 
-.table > thead {
+.table>thead {
   vertical-align: bottom;
   color: gray;
   font-size: 12px;

4c6d217df55d78e1b95cb814b54fc7fcc05ffe81
Muhammad Saud Khan <muhammadsaud.khan@cgi.com>
2022-08-18 14:42:53 +0200

    check access to CX shared service

--- src/services/service.const.js
+++ src/services/service.const.js
@@ -4,14 +4,29 @@ const MOCK_AUTH_URL = "https://mock--server.herokuapp.com";
 const GOOGLE_CHART_API_URL = "https://chart.googleapis.com";
 const DUMMY_SERVICE = "http://localhost:3000";
 
+const BASE_URL = "https://semantics.int.demo.catena-x.net";
+
+
+// for production 
 const INIT_OPTIONS = {
-    url: 'http://localhost:8088/auth',
-    clientId: 'Cl13-CX-Battery',
+    url: 'https://centralidp.demo.catena-x.net/auth/',
+    clientId: 'Cl4-CX-DigitalTwin',
     realm: 'CX-Central',
     onLoad: 'login-required'
 };
 
-const REDIRECT_URI = "http://localhost:8080/";
+const REDIRECT_URI = "https://materialpass.int.demo.catena-x.net/";
+
+// for development use only
+/*const INIT_OPTIONS = {
+    url: 'http://localhost:8088/auth/',
+    clientId: 'Cl13-CX-Battery',
+    realm: 'CX-Central',
+    onLoad: 'login-required'
+};
+const REDIRECT_URI = "http://localhost:8082/";*/
 
 
-export {TWIN_REGISTRY_URL, AAS_PROXY_URL, MOCK_AUTH_URL, GOOGLE_CHART_API_URL, DUMMY_SERVICE, INIT_OPTIONS, REDIRECT_URI};
+
+
+export {TWIN_REGISTRY_URL, AAS_PROXY_URL, MOCK_AUTH_URL, GOOGLE_CHART_API_URL, DUMMY_SERVICE, INIT_OPTIONS, REDIRECT_URI, BASE_URL};

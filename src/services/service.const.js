8ed2210fa92237952851b3f45eccb69a4f252001
zynda-sustragil <david.zynda@partners.basf.com>
2022-08-22 14:55:45 +0200

    auto mode

--- src/services/service.const.js
+++ src/services/service.const.js
@@ -5,22 +5,28 @@ const GOOGLE_CHART_API_URL = "https://chart.googleapis.com";
 const DUMMY_SERVICE = "http://localhost:3000";
 const SERVER_URL = "https://materialpass.int.demo.catena-x.net";
 
-// production mode start
-const INIT_OPTIONS = {
-    url: 'https://centralidp.demo.catena-x.net/auth/',
-    clientId: 'Cl13-CX-Battery',
-    realm: 'CX-Central',
-    onLoad: 'login-required'
-};
-
-const REDIRECT_URI = "https://materialpass.int.demo.catena-x.net/";
-// production mode end
-
-// development mode start
-/*const INIT_OPTIONS = {
-    url: 'http://localhost:8088/auth/',
-const REDIRECT_URI = "http://localhost:8082/";*/
-// development end
-
-
-export {TWIN_REGISTRY_URL, AAS_PROXY_URL, MOCK_AUTH_URL, GOOGLE_CHART_API_URL, DUMMY_SERVICE, INIT_OPTIONS, REDIRECT_URI, SERVER_URL};
+let INIT_OPTIONS = {};
+let REDIRECT_URI = "";
+if (window.location.href.includes(SERVER_URL)) {
+    // production mode start
+    INIT_OPTIONS = {
+        url: 'https://centralidp.demo.catena-x.net/auth/',
+        clientId: 'Cl13-CX-Battery',
+        realm: 'CX-Central',
+        onLoad: 'login-required'
+    };
+    REDIRECT_URI = "https://materialpass.int.demo.catena-x.net/";
+    // production mode end
+}
+else {
+    // development mode start
+    INIT_OPTIONS = {
+        url: 'http://localhost:8088/auth/',
+        clientId: 'Cl13-CX-Battery',
+        realm: 'CX-Central',
+        onLoad: 'login-required'
+    };
+    REDIRECT_URI = "http://localhost:8080/";
+    // development end
+}
+export { TWIN_REGISTRY_URL, AAS_PROXY_URL, MOCK_AUTH_URL, GOOGLE_CHART_API_URL, DUMMY_SERVICE, INIT_OPTIONS, REDIRECT_URI, SERVER_URL };

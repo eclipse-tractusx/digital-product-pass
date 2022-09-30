ba640a333f9dfbc5540d19b00c00b433a0d52876
davidzynda <david.zynda@partners.basf.com>
2022-09-30 10:32:16 +0200

    lint fixes

--- cypress.config.js
+++ cypress.config.js
@@ -7,10 +7,4 @@ module.exports = defineConfig({
   //       },
   //   },
   chromeWebSecurity: false,
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
 });

42492d061798b98be366da701d36c21d031676f8
davidzynda <david.zynda@partners.basf.com>
2022-11-04 11:42:27 +0100

    QR code scanner header in progress

--- src/views/QRScanner.vue
+++ src/views/QRScanner.vue
@@ -1,26 +1,29 @@
 <template>
-  <qrcode-stream @init="onInit"></qrcode-stream>
-  <Footer />
+  <div class="qr-container">
+    <img :src="CatenaLogo" alt="logo" class="logo" />
+    <div class="header-container">
+      <div>back</div>
+      <div>Scan QR code</div>
+      <div>flash</div>
+    </div>
+    <div class="qr-frame"></div>
+    <qrcode-stream class="test" @init="onInit"></qrcode-stream>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { QrcodeStream } from "vue3-qrcode-reader";
-import Spinner from "@/components/Spinner.vue";
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
-import axios from "axios";
-import { AAS_PROXY_URL } from "@/services/service.const";
-// import { BASE_URL } from '@/services/service.const';
-import { inject } from "vue";
-
+import CatenaLogo from "../assets/logo.png";
 export default {
   name: "PassportView",
   components: {
     QrcodeStream,
-
-    Footer,
-    Spinner,
+  },
+  setup() {
+    return {
+      CatenaLogo,
+    };
   },
 
   data() {
@@ -33,38 +36,45 @@ export default {
 };
 </script>
 
-<style>
-.pass-container {
-  width: 76%;
-  margin: 0 12% 0 12%;
+<style scoped>
+.qr-container {
+  position: relative;
 }
-
-.spinner-container {
-  width: 100vw;
-  height: 100vh;
+.header-container {
   display: flex;
   justify-content: center;
   align-items: center;
+  position: absolute;
+  width: 100%;
+  height: 137px;
+  background-color: #fff;
+  opacity: 0.3;
+  z-index: 3;
+  box-shadow: 0 0 10px 0 black;
+}
+.logo {
+  position: absolute;
+  top: 40px;
+  left: 50px;
+  height: 49px;
+  z-index: 5;
+}
+.qr-frame {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 300px;
+  height: 300px;
+  border: 6px solid #b3cb2c;
+  z-index: 10;
 }
 
-.spinner {
-  margin: auto;
-
-  width: 8vh;
-
-  animation: rotate 3s infinite;
+.qrcode-stream-overlay {
+  position: absolute;
+  backdrop-filter: blur(9px);
 }
 
-@keyframes rotate {
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
-@media (max-width: 750px) {
-  .pass-container {
-    width: 100%;
-    margin: 0;
-  }
+.test {
 }
 </style>

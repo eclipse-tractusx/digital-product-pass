c3ddd42aa9678af0ac77b10f318366b9861a3a24
davidzynda <david.zynda@partners.basf.com>
2022-11-07 22:21:32 +0100

    full functionality of QRcode scanner

--- src/views/QRScanner.vue
+++ src/views/QRScanner.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="qr-container">
     <img :src="CatenaLogo" alt="logo" class="logo" />
+    <p>{{ error }}</p>
+    <p>{{ decodedString }}</p>
     <div class="header-container">
       <div>back</div>
       <div>Scan QR code</div>
-      <div>flash</div>
+      <div @click="torch = !torch">flash</div>
     </div>
     <div class="qr-frame"></div>
-    <qrcode-stream class="test" @init="onInit"></qrcode-stream>
+    <qrcode-stream
+      :torch="torch"
+      class="test"
+      @init="onInit"
+      @decode="onDecode"
+    ></qrcode-stream>
   </div>
 </template>
 
@@ -27,12 +34,46 @@ export default {
   },
 
   data() {
-    return {};
+    return {
+      error: "",
+      decodedString: "",
+      torch: false,
+    };
   },
   async created() {
     this.loading = false;
   },
-  methods: {},
+  methods: {
+    async onInit(promise) {
+      try {
+        await promise;
+      } catch (error) {
+        if (error.name === "NotAllowedError") {
+          this.error = "ERROR: you need to grant camera access permission";
+        } else if (error.name === "NotFoundError") {
+          this.error = "ERROR: no camera on this device";
+        } else if (error.name === "NotSupportedError") {
+          this.error = "ERROR: secure context required (HTTPS, localhost)";
+        } else if (error.name === "NotReadableError") {
+          this.error = "ERROR: is the camera already in use?";
+        } else if (error.name === "OverconstrainedError") {
+          this.error = "ERROR: installed cameras are not suitable";
+        } else if (error.name === "StreamApiNotSupportedError") {
+          this.error = "ERROR: Stream API is not supported in this browser";
+        } else if (error.name === "InsecureContextError") {
+          this.error =
+            "ERROR: Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.";
+        } else {
+          this.error = `ERROR: Camera error (${error.name})`;
+        }
+      }
+    },
+    onDecode(decodedString) {
+      this.decodedString = decodedString;
+      console.log(decodedString);
+      window.location.replace(decodedString);
+    },
+  },
 };
 </script>
 

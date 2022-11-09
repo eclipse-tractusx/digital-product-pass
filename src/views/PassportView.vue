0880c38f763bdcbdb31ffb99adf2bedadde6ee84
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2022-11-09 15:46:25 +0100

    Removed test unused jwtToken

--- src/views/PassportView.vue
+++ src/views/PassportView.vue
@@ -72,8 +72,6 @@ export default {
     };
   },
   async created() {
-    let jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-    console.log(jwtToken);
     //   let assetIds = this.$route.params.assetIds;
     this.data = passportData;
     this.loading = false;

fc1877248496b949da83773218623e069c21973c
davidzynda <david.zynda@partners.basf.com>
2022-11-22 10:33:30 +0100

    update passport view

---
+++ src/views/PassportView.vue
@@ -0,0 +1,152 @@
+<template>
+  <Spinner v-if="loading" class="spinner-container" />
+  <div v-else>
+    <Header :battery-id="data" />
+    <div class="pass-container">
+      <GeneralInformation
+        section-title="General information"
+        :general-information="data"
+      />
+      <CellChemistry
+        section-title="Cell chemistry"
+        :cell-chemistry="data.cellChemistry"
+      />
+      <ElectrochemicalProperties
+        section-title="State of Health"
+        :electrochemical-properties="data.electrochemicalProperties"
+      />
+      <BatteryComposition
+        section-title="Parameters of The Battery"
+        :battery-composition="data.composition"
+      />
+      <StateOfBattery
+        section-title="State of Battery"
+        :state-of-battery="data"
+      />
+
+      <Documents section-title="Documents" :documents="data.document" />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GeneralInformation from "@/components/GeneralInformation.vue";
+import CellChemistry from "@/components/CellChemistry.vue";
+import ElectrochemicalProperties from "@/components/ElectrochemicalProperties.vue";
+import BatteryComposition from "@/components/BatteryComposition.vue";
+import StateOfBattery from "@/components/StateOfBattery.vue";
+import Documents from "@/components/Documents.vue";
+import Spinner from "@/components/Spinner.vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import { API_KEY } from "@/services/service.const";
+import apiWrapper from "@/services/wrapper";
+import AAS from "@/services/aasServices";
+import { inject } from "vue";
+export default {
+  name: "PassportView",
+  components: {
+    Header,
+    GeneralInformation,
+    CellChemistry,
+    StateOfBattery,
+    ElectrochemicalProperties,
+    BatteryComposition,
+    Documents,
+    Footer,
+    Spinner,
+  },
+  data() {
+    return {
+      auth: inject("authentication"),
+      data: null,
+      loading: true,
+      errors: [],
+      passId: this.$route.params.id,
+    };
+  },
+  async created() {
+    //this.loading = false;
+    //let assetIds = this.$route.params.assetIds;
+    this.data = await this.getPassport(this.passId);
+    this.loading = false;
+  },
+  methods: {
+    async getPassport(assetId) {
+      let assetIdJson = [{ key: "Battery_ID_DMC_Code", value: assetId }];
+      let aas = new AAS();
+      let wrapper = new apiWrapper();
+      let accessToken = await this.auth.getAuthTokenForTechnicalUser();
+      let AASRequestHeader = {
+        Authorization: "Bearer " + accessToken,
+      };
+
+      const shellId = await aas.getAasShellId(
+        JSON.stringify(assetIdJson),
+        AASRequestHeader
+      );
+      const shellDescriptor = await aas.getShellDescriptor(
+        shellId[0],
+        AASRequestHeader
+      );
+      const subModel = await aas.getSubmodelDescriptor(
+        shellDescriptor,
+        AASRequestHeader
+      );
+      if (subModel.endpoints.length > 0) {
+        let providerConnector = {
+          connectorAddress:
+            subModel.endpoints[0].protocolInformation.endpointAddress,
+          idShort: subModel.idShort,
+        };
+        let APIWrapperRequestHeader = {
+          "x-api-key": API_KEY,
+        };
+        //let assetId = JSON.parse(assetIds)[1].value; // Two elements in json array [batteryIDDMCode, assetId], get the last element and it wll always be the asset id i.e., [1]
+        console.info("Selected asset Id: " + assetId);
+        const response = await wrapper.performEDCDataTransfer(
+          assetId,
+          providerConnector,
+          APIWrapperRequestHeader
+        );
+        return response;
+      } else
+        alert(
+          "There is no connector endpoint defined in submodel.. Could not proceed further!"
+        );
+    },
+  },
+};
+</script>
+
+<style>
+.pass-container {
+  width: 76%;
+  margin: 0 12% 0 12%;
+}
+.spinner-container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.spinner {
+  margin: auto;
+  width: 8vh;
+  animation: rotate 3s infinite;
+}
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+@media (max-width: 750px) {
+  .pass-container {
+    width: 100%;
+    margin: 0;
+  }
+}
+</style>

7c740dd49a5f84822a07b8674cb4f095f507a04d
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2023-01-16 15:48:45 +0100

    Added api mapping to rest template

--- consumer-backend/productpass/src/test/java/tools/envToolsTest.java
+++ consumer-backend/productpass/src/test/java/tools/envToolsTest.java
@@ -11,34 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-class ApiControllerTest {
-    @Autowired
-    ApiController controller;
-
-
-    String assetId;
-    String idType;
-    Integer index;
-
-    @BeforeEach
-    void setUp() {
-        assetId = "X123456789012X12345678901234566";
-        idType = "Battery_ID_DMC_Code";
-        index = 0;
-    }
-    @Test
-    void getContractOfferByAssetId() {
-        try {
-            String date = dateTimeTools.getDateTimeFormatted(null);
-            Offer offer = controller.getContractOfferByAssetId(this.assetId);
-            Assertions.assertNotNull(offer);
-            logTools.printTest("["+this.getClass().getName()+".getContractOfferByAssetId] " + "Offer found: " + offer.getId() + " for assetId: ["+assetId+"]" );
-        } catch (ControllerException e) {
-            fail("It was not possible to retrieve contract offer");
-        }
-    }
-}
-@SpringBootTest
 class envToolsTest {
     public envTools env;
 

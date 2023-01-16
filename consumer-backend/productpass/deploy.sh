0c2b3e7241bc7c6dd5b9dd19efd6f6522e29d81e
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2023-01-16 17:08:01 +0100

    Updated deploy.sh

--- consumer-backend/productpass/deploy.sh
+++ consumer-backend/productpass/deploy.sh
@@ -17,8 +17,12 @@ then
     exit -1
 fi
 
-
+echo "[INFO] Building docker image for version: $version..."
 docker build -t ghcr.io/catenax-ng/product-battery-passport-consumer-app/product-pass-consumer-backend:$version .
+
+echo "[INFO] Tagging docker image version: $version..."
 docker tag ghcr.io/catenax-ng/product-battery-passport-consumer-app/product-pass-consumer-backend:$version ghcr.io/catenax-ng/product-battery-passport-consumer-app/product-pass-consumer-backend:$version
+
+echo "[INFO] Pushing docker image to repo: $version..."
 docker push ghcr.io/catenax-ng/product-battery-passport-consumer-app/product-pass-consumer-backend:$version
 

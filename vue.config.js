6d1498bea8817c64c1ab9df20bbd0634a503597e
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2023-01-18 19:34:53 +0100

    CHanged path

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: './',
+  publicPath: './passport/',
 
   devServer: {
     allowedHosts: 'all',

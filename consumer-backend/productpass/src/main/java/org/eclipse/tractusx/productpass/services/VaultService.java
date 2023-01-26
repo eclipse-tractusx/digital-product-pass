f9936bb9469c071c76a7aa3a42da00b596075699
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2023-01-26 15:40:46 +0100

    Added dependecies file from frontend and removed vault connection dependecy from backendç

--- consumer-backend/productpass/src/main/java/org/eclipse/tractusx/productpass/services/VaultService.java
+++ consumer-backend/productpass/src/main/java/org/eclipse/tractusx/productpass/services/VaultService.java
@@ -31,8 +31,6 @@ import org.eclipse.tractusx.productpass.exceptions.ServiceInitializationExceptio
 import org.eclipse.tractusx.productpass.models.service.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.vault.core.VaultTemplate;
-import org.springframework.vault.support.VaultResponse;
 import utils.*;
 
 import java.util.ArrayList;
@@ -42,53 +40,9 @@ import java.util.Map;
 @Service
 public class VaultService extends BaseService {
     public static final ConfigUtil configuration = new ConfigUtil();
-    @Autowired
-    private VaultTemplate vaultTemplate;
-
     public VaultService() throws ServiceInitializationException {
         this.checkEmptyVariables();
     }
-
-    public Object mapSecret(String secretPath, Class<?> ClassType) {
-        try {
-            VaultResponse vaultResponse = vaultTemplate.read(secretPath);
-            if(vaultResponse == null){
-                throw new ServiceException(this.getClass().getName()+"."+"getSecret",
-                        "It was not possible to get secret from vault. Vault Response is null!");
-            }
-            Object data = vaultResponse.getData();
-            if(data == null){
-                throw new ServiceException(this.getClass().getName()+"."+"getSecret",
-                        "It was not possible to get secret from vault. Data is null!");
-            }
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.convertValue(data, ClassType);
-        }catch (Exception e){
-            throw new ServiceException(this.getClass().getName()+"."+"mapSecret",
-                    e,
-                    "It was not possible to map secret from vault.");
-        }
-    }
-    public Object getSecret(String secretPath) {
-        try {
-            VaultResponse vaultResponse = vaultTemplate.read(secretPath);
-            if(vaultResponse == null){
-                throw new ServiceException(this.getClass().getName()+"."+"getSecret",
-                        "It was not possible to get secret from vault. Vault Response is null!");
-            }
-            Map<?, ?> data = vaultResponse.getData();
-            if(data == null){
-                throw new ServiceException(this.getClass().getName()+"."+"getSecret",
-                        "It was not possible to get secret from vault. Data is null!");
-            }
-            return data.get("data");
-        }catch (Exception e){
-            throw new ServiceException(this.getClass().getName()+"."+"getSecret",
-                    e,
-                    "It was not possible to get secret from vault.");
-        }
-    }
-
     public Object getLocalSecret(String localSecretPath) {
         try {
             String secret = null;

da01366ad0a38830d066a8564cbd32de28364c3c
davidzynda <david.zynda@partners.basf.com>
2023-02-14 16:25:27 +0100

    enter key bug fixed

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -31,9 +31,30 @@
         <p class="text">Turn it on or type the ID.</p>
         <p class="error">{{ error }}</p>
       </div>
-      <SearchInput />
+      <v-form class="form">
+        <div class="input-form">
+          <input
+            v-model="typedCode"
+            class="input"
+            type="text"
+            placeholder="Type ID"
+          />
+        </div>
+        <v-btn
+          rounded="pill"
+          color="#0F71CB"
+          size="small"
+          class="submit-btn"
+          @click="onClick"
+          type="submit"
+        >
+          Search
+          <v-icon class="icon" start md icon="mdi-arrow-right"></v-icon>
+        </v-btn>
+      </v-form>
     </div>
     <div class="qr-container" data-cy="qr-container">
+      <router-link to="/dashboard"> </router-link>
       <div v-if="!error">
         <div v-if="QRtoggle">
           <div class="qr-frame">
@@ -47,7 +68,27 @@
           ></qrcode-stream>
         </div>
         <div v-else class="qr-container">
-          <SearchInput />
+          <v-form class="form" @submit.prevent>
+            <div class="input-form">
+              <input
+                v-model="typedCode"
+                class="input"
+                type="text"
+                placeholder="Type ID"
+              />
+            </div>
+            <v-btn
+              rounded="pill"
+              color="#0F71CB"
+              size="small"
+              class="submit-btn"
+              @click="onClick"
+              type="submit"
+            >
+              Search
+              <v-icon class="icon" start md icon="mdi-arrow-right"></v-icon>
+            </v-btn>
+          </v-form>
         </div>
       </div>
     </div>
@@ -57,31 +98,62 @@
 <script>
 import { QrcodeStream } from "vue3-qrcode-reader";
 import CatenaLogo from "../media/logo.png";
+import Flesh from "../media/flesh.svg";
+import Close from "../media/close.svg";
 import QRFrame from "../media/qrFrame.svg";
-import SearchInput from "../components/general/SearchInput.vue";
+import Search from "../media/qrSearch.svg";
+import Logout from "../media/logout.png";
+import Profile from "../media/profile.svg";
+import Notifications from "../media/notifications.svg";
+import Settings from "../media/settings.svg";
+import { inject } from "vue";
 
 export default {
   name: "QRScannerView",
   components: {
     QrcodeStream,
-    SearchInput,
   },
   setup() {
-    SearchInput;
     return {
       CatenaLogo,
+      Flesh,
+      Close,
+      Search,
       QRFrame,
+      Profile,
+      Notifications,
+      Settings,
+      Logout,
     };
   },
 
   data() {
     return {
+      hover: false,
       QRtoggle: false,
       error: "",
       decodedString: "",
+      torch: false,
+      MATERIAL_URL: process.env.VUE_APP_MATERIAL_URL,
+      typedCode: "",
+      username: "",
+      role: "",
+      auth: inject("authentication"),
     };
   },
+  mounted() {
+    if (this.auth.isUserAuthenticated) {
+      this.username = this.auth.getUserName();
+      this.role = this.auth.getRole();
+    }
+  },
+  async created() {
+    this.loading = false;
+  },
   methods: {
+    logout() {
+      this.auth.logout();
+    },
     async onInit(promise) {
       try {
         await promise;
@@ -112,11 +184,22 @@ export default {
         path: `/${decodedString}`,
       });
     },
+
+    onClick() {
+      this.$router.push({
+        path: `/${this.typedCode}`,
+      });
+      console.log("clicked");
+    },
   },
 };
 </script>
 
 <style scoped>
+.icon {
+  padding-left: 20px;
+}
+
 .switch-container {
   display: flex;
   justify-content: flex-end;
@@ -159,6 +242,47 @@ export default {
   z-index: 10;
 }
 
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.input-form {
+  padding: 17px;
+  background: linear-gradient(to right, #f8b500, #f88000);
+  border-radius: 35px;
+}
+.input {
+  width: 560px;
+  border-radius: 20px;
+  color: #444;
+  padding: 18px;
+  padding-left: 60px;
+  font-size: 20px;
+  outline: none;
+  background-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiNhMGEwYTA7fS5jbHMtMntmaWxsOiNhMGEwYTA7fTwvc3R5bGU+PC9kZWZzPjxwYXRoIGNsYXNzPSJjbHMtMSIgZD0iTTE1LjUgMTRoLS43OWwtLjI4LS4yN0MxNS40MSAxMi41OSAxNiAxMS4xMSAxNiA5LjUgMTYgNS45MSAxMy4wOSAzIDkuNSAzUzMgNS45MSAzIDkuNSA1LjkxIDE2IDkuNSAxNmMxLjYxIDAgMy4wOS0uNTkgNC4yMy0xLjU3bC4yNy4yOHYuNzlsNSA0Ljk5TDIwLjQ5IDE5bC00Ljk5LTV6bS02IDBDNy4wMSAxNCA1IDExLjk5IDUgOS41UzcuMDEgNSA5LjUgNSAxNCA3LjAxIDE0IDkuNSAxMS45OSAxNCA5LjUgMTR6Ii8+PC9zdmc+Cg==);
+  background-repeat: no-repeat;
+  background-position: 16px center;
+  background-size: 32px 32px;
+  border: 1px solid lightgray;
+  border-radius: 25px;
+  background-color: white;
+}
+
+.submit-btn {
+  margin-top: 30px;
+  height: 56px;
+  width: 185px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
 p {
   font-size: 14px;
   font-weight: 500;
@@ -181,18 +305,20 @@ p {
     width: 250px;
     height: 250px;
   }
-
   .qr-frame {
     width: 250px;
     height: 250px;
   }
-
   .qr-frame {
     position: absolute !important;
     top: 70% !important;
     left: 50% !important;
     transform: translate(-50%, -50%) !important;
     z-index: 10 !important;
+  }
+  .input {
+    position: relative;
+    width: 50vw;
   }
 }
 @media (max-width: 670px) {
@@ -208,7 +334,6 @@ p {
     width: 150px;
     height: 150px;
   }
-
   .qr-frame {
     width: 150px;
     height: 150px;
@@ -221,6 +346,13 @@ p {
     left: 50% !important;
     transform: translate(-50%, -50%);
     z-index: 10;
+  }
+}
+
+@media (max-width: 375px) {
+  .input {
+    position: relative;
+    width: 80vw;
   }
 }
 </style>

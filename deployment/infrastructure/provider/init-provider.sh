a08738c7cd3bcea76b6af885bd3e5cbdaed570b5
Muhammad Saud Khan <muhammadsaud.khan@cgi.com>
2023-03-16 12:28:53 +0100

    upgrade postman collections and resources to align with v3.0.1 data model for dev, int and beta

--- deployment/infrastructure/provider/init-provider.sh
+++ deployment/infrastructure/provider/init-provider.sh
@@ -27,6 +27,7 @@ DIGITAL_TWIN_SUBMODEL_ID_2='urn:uuid:49a06ad2-64b7-46c8-9f3b-a718c462ca23'
 
 DIGITAL_TWIN_3='urn:uuid:365e6fbe-bb34-11ec-8422-0242ac120002'
 DIGITAL_TWIN_SUBMODEL_ID_3='urn:uuid:61125dc3-5e6f-4f4b-838d-447432b97918'
+BPN='BPNL000000000000'
 
 SERVER_URL='https://materialpass.int.demo.catena-x.net'
 REGISTRY_URL='https://semantics.int.demo.catena-x.net/registry/registry/shell-descriptors'
@@ -36,28 +37,30 @@ REGISTRY_URL='https://semantics.int.demo.catena-x.net/registry/registry/shell-de
 BEARER_TOKEN=''
 
 API_KEY=''
-
+ASSET_ID_1=${DIGITAL_TWIN_1}-${DIGITAL_TWIN_SUBMODEL_ID_1}
+ASSET_ID_2=${DIGITAL_TWIN_2}-${DIGITAL_TWIN_SUBMODEL_ID_2}
+ASSET_ID_3=${DIGITAL_TWIN_3}-${DIGITAL_TWIN_SUBMODEL_ID_3}
 
 echo '**************************Asset 1 **********************'
 echo
 # Create Submodel data
 echo "Create sample data for asset 1..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/payloads/X123456789012X12345678901234566.json"  $SERVER_URL/provider_backend/data/$DIGITAL_TWIN_SUBMODEL_ID_1
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/payloads/X123456789012X12345678901234566.json"  $SERVER_URL/provider_backend/data/${ASSET_ID_1}
 echo
 
 # Create a asset
 echo "Create asset 1..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/assets/X123456789012X12345678901234566.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/assets
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/assets/X123456789012X12345678901234566.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/assets
 echo
 
 # Create a general policy
 echo "Create policy for asset 1..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractpolicies/X123456789012X12345678901234566.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/policydefinitions
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractpolicies/X123456789012X12345678901234566.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/policydefinitions
 echo
 
 # Create a contract definition
 echo "Create contract definition for asset 1..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractdefinitions/X123456789012X12345678901234566.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/contractdefinitions
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractdefinitions/X123456789012X12345678901234566.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/contractdefinitions
 echo
 
 # Create a digital twin and register inside CX registry
@@ -75,22 +78,22 @@ echo '**************************Asset 2 **********************'
 echo 
 # Create Submodel data
 echo "Create sample data for asset 2..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/payloads/NCR186850B.json"  $SERVER_URL/provider_backend/data/$DIGITAL_TWIN_SUBMODEL_ID_2
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/payloads/NCR186850B.json"  $SERVER_URL/provider_backend/data/${ASSET_ID_2}
 echo
 
 # Create a asset
 echo "Create asset 2..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/assets/NCR186850B.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/assets
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/assets/NCR186850B.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/assets
 echo
 
 # Create a general policy
 echo "Create policy for asset 2..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractpolicies/NCR186850B.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/policydefinitions
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractpolicies/NCR186850B.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/policydefinitions
 echo
 
 # Create a contract definition
 echo "Create contract definition for asset 2..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractdefinitions/NCR186850B.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/contractdefinitions
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractdefinitions/NCR186850B.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/contractdefinitions
 echo
 
 
@@ -107,22 +110,22 @@ echo
 echo '**************************Asset 3 **********************'
 # Create Submodel data
 echo "Create sample data for asset 3..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/payloads/IMR18650V1.json"   $SERVER_URL/provider_backend/data/$DIGITAL_TWIN_SUBMODEL_ID_3
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/payloads/IMR18650V1.json"   $SERVER_URL/provider_backend/data/${ASSET_ID_3}
 echo
 
 # Create a asset
 echo "Create asset 1..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/assets/IMR18650V1.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/assets
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/assets/IMR18650V1.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/assets
 echo
 
 # Create a general policy
 echo "Create policy for asset 3..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractpolicies/IMR18650V1.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/policydefinitions
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractpolicies/IMR18650V1.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/policydefinitions
 echo
 
 # Create a contract definition
 echo "Create contract definition for asset 3..."
-curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractdefinitions/IMR18650V1.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/provider/data/contractdefinitions
+curl -X POST -H 'Content-Type: application/json' -s --data "@resources/contractdefinitions/IMR18650V1.json" --header 'X-Api-Key: '${API_KEY} $SERVER_URL/${BPN}/data/contractdefinitions
 echo
 
 # Create a digital twin and register inside CX registry

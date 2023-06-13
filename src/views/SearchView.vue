6859c2341ac8f38b26af16e8e9487853b4c881f3
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2023-06-13 18:47:57 +0200

    feat: increased responsiveness, headers licenses and css details

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,3 +1,26 @@
+<!--
+  Catena-X - Product Passport Consumer Frontend
+ 
+  Copyright (c) 2022, 2023 BASF SE, BMW AG, Henkel AG & Co. KGaA
+ 
+  See the NOTICE file(s) distributed with this work for additional
+  information regarding copyright ownership.
+ 
+  This program and the accompanying materials are made available under the
+  terms of the Apache License, Version 2.0 which is available at
+  https://www.apache.org/licenses/LICENSE-2.0.
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+  either express or implied. See the
+  License for the specific language govern in permissions and limitations
+  under the License.
+ 
+  SPDX-License-Identifier: Apache-2.0
+-->
+
+
 <template>
   <div class="home-page-container">
     <div class="left-container" :class="{ hidden: isHidden }">
@@ -18,69 +41,71 @@
       </div>
     </div>
     <div class="right-container">
-      <div class="logotype-container">
-        <img :src="LogotypeDPP" alt="DPP logo" />
-      </div>
-
-      <v-icon
-        @click="showWelcome"
-        size="large"
-        :class="{ hidden: !isHidden }"
-        class="arrow-icon"
-        icon="mdi-arrow-right"
-      ></v-icon>
-
-      <v-icon
-        @click="hideWelcome"
-        size="large"
-        :class="{ hidden: isHidden }"
-        class="arrow-icon"
-        icon="mdi-arrow-left"
-      ></v-icon>
-
-      <v-container class="search-page">
-        <div v-if="error" class="qr-container">
-          <div class="text-container">
-            <p class="text">Your camera is off.</p>
-            <p class="text">Turn it on or type the ID.</p>
-            <p class="error">{{ error }}</p>
-          </div>
-          <SearchInput class="search-input" />
+      <div class="inner-right-container">
+        <div class="logotype-container">
+          <img :src="LogotypeDPP" alt="DPP logo" />
         </div>
-        <v-row data-cy="qr-container">
-          <div v-if="!error">
-            <v-col class="qr-container" cols="12" v-if="QRtoggle">
-              <div class="stream-container">
+
+        <v-icon
+          @click="showWelcome"
+          size="large"
+          :class="{ hidden: !isHidden }"
+          class="arrow-icon"
+          icon="mdi-arrow-right"
+        ></v-icon>
+
+        <v-icon
+          @click="hideWelcome"
+          size="large"
+          :class="{ hidden: isHidden }"
+          class="arrow-icon"
+          icon="mdi-arrow-left"
+        ></v-icon>
+
+        <v-container class="search-page">
+          <div v-if="error" class="qr-container">
+            <div class="text-container">
+              <p class="text">Your camera is off.</p>
+              <p class="text">Turn it on or type the ID.</p>
+              <p class="error">{{ error }}</p>
+            </div>
+            <SearchInput class="search-input" />
+          </div>
+          <v-row data-cy="qr-container">
+            <div v-if="!error">
+              <v-col class="qr-container" cols="12" v-if="QRtoggle">
+                <div class="stream-container">
+                  <v-icon
+                    size="x-large"
+                    class="close-btn"
+                    @click="closeQRScanner"
+                    start
+                    md
+                    icon="mdi-close-thick"
+                  ></v-icon>
+                  <qrcode-stream
+                    :torch="torch"
+                    class="qrcode-stream"
+                    @init="onInit"
+                    @decode="onDecode"
+                  ></qrcode-stream>
+                </div>
+              </v-col>
+              <v-col cols="12" v-else class="qr-container">
+                <SearchInput class="search-input" />
                 <v-icon
-                  size="x-large"
-                  class="close-btn"
-                  @click="closeQRScanner"
+                  class="qrScanner-btn"
+                  :class="{ hidden: !isHidden }"
+                  @click="openQRScanner"
                   start
                   md
-                  icon="mdi-close-thick"
+                  icon="mdi-qrcode-scan"
                 ></v-icon>
-                <qrcode-stream
-                  :torch="torch"
-                  class="qrcode-stream"
-                  @init="onInit"
-                  @decode="onDecode"
-                ></qrcode-stream>
-              </div>
-            </v-col>
-            <v-col cols="12" v-else class="qr-container">
-              <SearchInput class="search-input" />
-              <v-icon
-                class="qrScanner-btn"
-                :class="{ hidden: !isHidden }"
-                @click="openQRScanner"
-                start
-                md
-                icon="mdi-qrcode-scan"
-              ></v-icon>
-            </v-col>
-          </div>
-        </v-row>
-      </v-container>
+              </v-col>
+            </div>
+          </v-row>
+        </v-container>
+      </div>
       <div class="guide">
         📖 Want to find out more? Read our Get
         <a class="advanced-search-link" @click="openExternalLink"

97297ddaad53b888a0fce10e771602573ca2ede0
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2023-08-10 16:38:53 +0200

    feat: ignore extra fields commings from request

--- consumer-backend/productpass/src/main/java/org/eclipse/tractusx/productpass/models/negotiation/Dataset.java
+++ consumer-backend/productpass/src/main/java/org/eclipse/tractusx/productpass/models/negotiation/Dataset.java
@@ -23,11 +23,13 @@
 
 package org.eclipse.tractusx.productpass.models.negotiation;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Dataset extends DidDocument{
     @JsonProperty("odrl:hasPolicy")

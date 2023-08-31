630c28d70c6701e07ec503c71b737802c4b52d01
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2023-08-31 11:13:01 +0200

    fix: added anotation to ignore extra fields comming from the dtr

--- consumer-backend/productpass/src/main/java/org/eclipse/tractusx/productpass/models/dtregistry/SubModel3.java
+++ consumer-backend/productpass/src/main/java/org/eclipse/tractusx/productpass/models/dtregistry/SubModel3.java
@@ -23,11 +23,16 @@
 
 package org.eclipse.tractusx.productpass.models.dtregistry;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.ArrayList;
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SubModel3 {
     @JsonProperty("description")
     ArrayList<JsonNode> description;

413b5fbc3c5abf2eca2c3ddad63d17238e90a9b4
Parracho <diogo.parracho@cgi.com>
2023-10-25 09:32:40 +0100

    feat: merged develop last updates

---
+++ consumer-backend/productpass/src/test/java/org/eclipse/tractusx/productpass/managers/ProcessManagerTest.java
@@ -0,0 +1,64 @@
+package org.eclipse.tractusx.productpass.managers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProcessManagerTest {
+
+    @Test
+    void loadDataModel() {
+    }
+
+    @Test
+    void saveDataModel() {
+    }
+
+    @Test
+    void getProcess() {
+    }
+
+    @Test
+    void generateStatusToken() {
+    }
+
+    @Test
+    void generateToken() {
+    }
+
+    @Test
+    void checkProcess() {
+    }
+
+    @Test
+    void testCheckProcess() {
+    }
+
+    @Test
+    void startNegotiation() {
+    }
+
+    @Test
+    void setProcess() {
+    }
+
+    @Test
+    void setProcessState() {
+    }
+
+    @Test
+    void getProcessState() {
+    }
+
+    @Test
+    void createProcess() {
+    }
+
+    @Test
+    void testCreateProcess() {
+    }
+
+    @Test
+    void deleteSearchDir() {
+    }
+}
--- consumer-backend/productpass/src/test/java/org/eclipse/tractusx/productpass/services/AasServiceTest.java
+++ consumer-backend/productpass/src/test/java/org/eclipse/tractusx/productpass/services/AasServiceTest.java
@@ -1,4 +1,52 @@
+package org.eclipse.tractusx.productpass.services;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AasServiceTest {
-  
+
+    @Test
+    void searchDigitalTwin() {
+    }
+
+    @Test
+    void getPathEndpoint() {
+    }
+
+    @Test
+    void searchSubModelBySemanticId() {
+    }
+
+    @Test
+    void testSearchSubModelBySemanticId() {
+    }
+
+    @Test
+    void getDigitalTwin() {
+    }
+
+    @Test
+    void getSubModelBySemanticId() {
+    }
+
+    @Test
+    void testGetSubModelBySemanticId() {
+    }
+
+    @Test
+    void getTokenHeader() {
+    }
+
+    @Test
+    void getRegistryUrl() {
+    }
+
+    @Test
+    void decentralDtrSearch() {
+    }
+
+    @Test
+    void queryDigitalTwin() {
+    }
 }

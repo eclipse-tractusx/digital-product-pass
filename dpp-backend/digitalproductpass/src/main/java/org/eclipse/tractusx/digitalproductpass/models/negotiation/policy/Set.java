ac5a7f6c26e79e783532da6195e718545539352c
Mathias Brunkow Moser <mathias.brunkowmoser@cgi.com>
2024-05-10 19:12:38 +0200

    chore: updated bug from lombok

--- dpp-backend/digitalproductpass/src/main/java/org/eclipse/tractusx/digitalproductpass/models/negotiation/policy/Set.java
+++ dpp-backend/digitalproductpass/src/main/java/org/eclipse/tractusx/digitalproductpass/models/negotiation/policy/Set.java
@@ -30,8 +30,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Singular;
+import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 import org.eclipse.tractusx.digitalproductpass.config.PolicyCheckConfig;
 import org.eclipse.tractusx.digitalproductpass.exceptions.ModelException;
@@ -48,7 +51,7 @@ import java.util.Objects;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Jacksonized
-@Builder
+@JsonPOJOBuilder(buildMethodName = "createSet", withPrefix = "construct")
 public class Set extends DidDocument {
 
     /** ATTRIBUTES **/
@@ -62,6 +65,9 @@ public class Set extends DidDocument {
     @JsonProperty("odrl:obligation")
     Collection<Action> obligations;
 
+    public Set createSet() {
+        return new Set(permissions, prohibitions, obligations);
+    }
     /** CONSTRUCTOR(S) **/
     public Set(String id, String type, Collection<Action> permissions, Collection<Action> prohibitions, Collection<Action> obligations) {
         super(id, type);

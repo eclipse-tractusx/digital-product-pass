c074c17927e62ddb7eadeff32eb634ec1734fe15
Muhammad Saud Khan <muhammadsaud.khan@cgi.com>
2024-06-10 16:54:31 +0200

    feat(dpp-verification-backend): prepare Springboot Maven project for the dpp-verification

---
+++ dpp-verification/src/main/java/org/eclipse/tractusx/verification/Application.java
@@ -0,0 +1,47 @@
+/*********************************************************************************
+ *
+ * Tractus-X - Digital Product Passport Verification Add-on
+ *
+ * Copyright (c) 2022 BMW AG
+ * Copyright (c) 2023 CGI Deutschland B.V. & Co. KG
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the
+ * License for the specific language govern in permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+package org.eclipse.tractusx.verification;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * This class is the main class of the Application, where the main method is to start it with Spring boot.
+ **/
+@SpringBootApplication
+@EnableAsync
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication application =
+                new SpringApplication(Application.class);
+        application.run(args);
+        System.out.println("Hello world!");
+    }
+}
